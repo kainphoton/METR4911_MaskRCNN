c4a2b55884b7ea429e7ae852c6cec3547e9fec19
kainphoton <luckyphotonkain@protonmail.com>
2022-11-04 11:14:44 +1000

    Add apple result plot images

--- METR4911_package/__init__.py
+++ METR4911_package/__init__.py
@@ -18,7 +18,7 @@ SCORE_THRESHOLD = 0.75
 
 ############################ Global variables used in Item() and Finger(Item) class ############################
 # Using Median Adjustment Values: 4/10/22
-ITEM_VOLUME_ADJUSTMENT = {"Banana": 2.205, "Apple": 2.3125, "Carrot": 1.0, "Orange": 1.67, "Broccoli": 1.0}
+ITEM_VOLUME_ADJUSTMENT = {"Banana": 2.205, "Apple": 2.78, "Carrot": 1.0, "Orange": 1.67, "Broccoli": 1.0}
 
 # Shape Category [different shape will have their own volume equation]
 SPHERE_SHAPE = ['Apple', 'Orange']
